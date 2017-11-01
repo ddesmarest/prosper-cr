43e4104f9a69bb036bb3a810eb9364c7ebc282fd
David Desmarest <ddesmarest@gmail.com>
2017-11-01 23:51:20 +0100

    Fixing script

--- run_python_tests.sh
+++ run_python_tests.sh
@@ -1,13 +1,10 @@
-
 #!/bin/bash
-
-#fullfile=`realpath $0`
-#d=$(dirname $fullfile)
-export PYTHONPATH=src
+fullfile=`realpath $0`
+d=$(dirname $fullfile)
+PYTHONPATH=$d/src
 echo $PYTHONPATH
-tree
 #nosetests  --processes=1 --process-restartworker --with-cov --cov src/server --cov-config=src/server/.coveragerc src/server $@
-nosetests  --processes=0 --with-cov --cov src/server --cov-config=src/server/.coveragerc src/server $@
+nosetests  --processes=0 --with-cov --cov $d/src/server --cov-config=$d/src/server/.coveragerc $d/src/server $@
 result=$?
 mongo tools/list_db.js
 exit $result
