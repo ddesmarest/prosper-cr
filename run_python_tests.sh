d4a699bcb47cee037c76035f4e8503f14b1337a8
David Desmarest <ddesmarest@gmail.com>
2017-11-01 19:28:43 +0100

    Test for build issue

--- run_python_tests.sh
+++ run_python_tests.sh
@@ -3,6 +3,7 @@ fullfile=`realpath $0`
 d=$(dirname $fullfile)
 export PYTHONPATH=src
 echo $PYTHONPATH
+tree
 #nosetests  --processes=1 --process-restartworker --with-cov --cov src/server --cov-config=src/server/.coveragerc src/server $@
 nosetests  --processes=0 --with-cov --cov $d/src/server --cov-config=$d/src/server/.coveragerc $d/src/server $@
 result=$?
