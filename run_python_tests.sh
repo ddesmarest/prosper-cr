a1c5ca26613a314794529c093fe2009fc5a5c547
David Desmarest <ddesmarest@gmail.com>
2017-11-01 19:15:20 +0100

    Adding message

--- run_python_tests.sh
+++ run_python_tests.sh
@@ -2,6 +2,7 @@
 fullfile=`realpath $0`
 d=$(dirname $fullfile)
 PYTHONPATH=$d/src
+echo $PYTHONPATH
 #nosetests  --processes=1 --process-restartworker --with-cov --cov src/server --cov-config=src/server/.coveragerc src/server $@
 nosetests  --processes=0 --with-cov --cov $d/src/server --cov-config=$d/src/server/.coveragerc $d/src/server $@
 result=$?
