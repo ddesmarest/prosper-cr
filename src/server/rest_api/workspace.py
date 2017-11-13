86025a002e69394a6fabb8c1b36414266ad93403
David Desmarest <ddesmarest@gmail.com>
2017-11-13 13:45:20 +0100

    Avoid code duplication

--- src/server/rest_api/workspace.py
+++ src/server/rest_api/workspace.py
@@ -41,6 +41,13 @@ class WorkspaceAPI(Resource):
     """
     /workspaces/<workspace id> handler
     """
+
+    def get_workspace(self, workspace_id):
+        """
+        return the workspace given its id
+        """
+        return Workspace.objects(users=g.user, id=workspace_id)[0]
+
     @AUTH.login_required
     def get(self, workspace_id):
         """
@@ -50,7 +57,7 @@ class WorkspaceAPI(Resource):
         parser.add_argument('recursive', type=bool, default=False)
         args = parser.parse_args()
         try:
-            workspace = Workspace.objects(users=g.user, id=workspace_id)[0]
+            workspace = self.get_workspace(workspace_id)
             return workspace.to_dict(args['recursive'])
         except Exception as e:
             return create_error_data('Workspace cannot be found for user ' + g.user.email, e), 400
@@ -67,7 +74,7 @@ class WorkspaceAPI(Resource):
 
         try:
             new_name = args['name']
-            workspace = Workspace.objects(users=g.user, id=workspace_id)[0]
+            workspace = workspace = self.get_workspace(workspace_id)
             if workspace.name != new_name:
                 workspace.update(set__name=new_name)
                 workspace.reload()
@@ -81,7 +88,7 @@ class WorkspaceAPI(Resource):
         Delete a workspace
         """
         try:
-            workspace = Workspace.objects(users=g.user, id=workspace_id)[0]
+            workspace = workspace = self.get_workspace(workspace_id)
             workspace.delete()
             return {}, 204
         except Exception as e:
